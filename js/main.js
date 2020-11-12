72b51a93877d11d69e70e5db249ae8aac6439201
paula dixon <paulapdixon@gmail.com>
2020-11-12 13:35:02 -0800

    added colors to squares via js classlists

--- js/main.js
+++ js/main.js
@@ -61,3 +61,14 @@
 //         console.log('blue team')
 //     } else console.log ('red team')
 // }
+
+const squares = Array.from(document.querySelectorAll('.grid div'))
+
+
+for (let i=0; i< squares.length; i++){
+    if (i%2 === 0){
+        squares[i].classList.add('even')
+    } else {
+        squares[i].classList.add('odd')
+    }
+}
