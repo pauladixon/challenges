4d79e79de0e8f6ab0136eed8fb24e9b45165bb17
paula dixon <paulapdixon@gmail.com>
2020-11-12 11:39:41 -0800

    object js practice

--- js/main.js
+++ js/main.js
@@ -4,3 +4,17 @@ let x = function (a, b) {
     a + b == c
     return x
 }
+
+
+let paula = {
+    name: 'paula',
+    age: 30,
+    friends: ['joann', 'mattie', 'kent'],
+    isPetOwner: true,
+    greeting: 'sup',
+    sayGreeting: function(){
+        console.log(this.greeting)
+    }
+}
+
+
