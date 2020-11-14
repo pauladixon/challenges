604a52c3a63077f04b6e2e2a01e5c1268df10667
paula dixon <paulapdixon@gmail.com>
2020-11-13 21:48:29 -0800

    added conditional coloring for gameboard

--- js/main.js
+++ js/main.js
@@ -78,7 +78,13 @@ function createBoard() {
         squares.push(square)
 
         if (layout[i] === 0){
-            squares[i].classList.add(pac-dot)
+            squares[i].classList.add('pac-dot')
+        } else if (layout[i] === 1){
+            squares[i].classList.add('wall')
+        } else if (layout[i] === 2){
+            squares[i].classList.add('ghost-lair')
+        } else if (layout[i] === 3){
+            squares[i].classList.add('power-pellets')
         }
     }
 }
