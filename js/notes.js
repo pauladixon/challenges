2ddc085b16492755bb11b1fb76f4a7e51ac1529b
paula dixon <paulapdixon@gmail.com>
2020-11-14 11:57:16 -0800

    added fetch example for posting json data

--- js/notes.js
+++ js/notes.js
@@ -216,7 +216,22 @@ userData2
 
 
 
+
     
+
 fetch('https://jsonplaceholder.typicode.com/comments/1')
     .then(response => response.json())
     .then(data => console.log(data))
+
+
+fetch('https://jsonplaceholder.typicode.com/comments', {
+    method: "POST",
+    body: JSON.stringify({
+        postId: 1,
+        name: 'Dylan',
+        email: 'dylansemail310@gmail.com',
+        body: 'That was dope!'
+    })
+})
+.then(response => response.json())
+.then(data => console.log(data))
