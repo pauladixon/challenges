bbb1c4e4b236a8666407fce88c4771acd1c1cc9c
paula dixon <paulapdixon@gmail.com>
2020-11-24 12:49:37 -0500

    adding moveghost function

--- js/widgets.js
+++ js/widgets.js
@@ -133,6 +133,15 @@ const ghosts = [
 
 ghosts.forEach(ghost => squares[ghost.startIndex].classList.add(ghost.className))
 
+ghosts.forEach(ghost => moveGhost(ghost))
+
+function moveGhost(ghost) {
+    const directions = [-1, +1, -width, +width]
+    let direction = directions[Math.floor(Math.random() * directions.length)]
+    ghost.timerId = setInterval(function() {
+
+    } ghost.speed)
+}
 
 // move on element click - robot widget
 
